05d1868a17ae4ab13c797a85ec65241035568e6d
Pavarino Arthur <arthur.pavarino@etu.u-bordeaux.fr>
2017-11-19 15:44:18 +0100

    Ajout fonction de répartition de planètes dans l'espace

--- src/simpleUIApp/Main.java
+++ src/simpleUIApp/Main.java
@@ -12,35 +12,51 @@ public class Main {
 		Random random = new Random();
 		ArrayList<Item> testItemList = new ArrayList<Item>();
 		ArrayList<SpaceShip> testItem = new ArrayList<SpaceShip>();
-		Planet arthur = new Planet(random.nextInt(400), random.nextInt(500), 50, 1.0);
-		Planet marcus = new Planet(0, 0, 0, 1.0);
-		boolean wellPlaced = false;
-		while(!wellPlaced){
+		
+		// On génère une liste de planètes
+		ArrayList<Planet> listPlanets = new ArrayList<Planet>();
+		
+		// On initialise x planets
+		for(int i = 0; i < 5; i++)
+		{
+			Point2D p = Planet.findPlanetPosition(800, 600, listPlanets,50);
+			
+			if(p.getX() != -1)
+				listPlanets.add(new Planet(p.getX(), p.getY(), 50, 1.0));
+		}
+		
+		
+		//Planet arthur = new Planet(random.nextInt(400), random.nextInt(500), 100, 1.0);
+		//Planet marcus = new Planet(0, 0, 0, 1.0);
+		//boolean wellPlaced = false;
+		/*while(!wellPlaced){
 			int x, y;
 			x = random.nextInt(400);
 			y = random.nextInt(500);
-			if(arthur.getLocation().distance(new Point2D.Double(x, y)) <= 50){
+			if(arthur.getLocation().distance(new Point2D.Double(x, y)) <= 200){
 				System.out.println("Marcus too close");
 			}else{
 				marcus.getLocation().setLocation(x, y);
-				marcus.setWidth(50);
+				marcus.setWidth(100);
 				wellPlaced = true;
 			}
-		}
+		}*/
 		/*
 		 * Randomly position 25*2 Ships in the Arena zone (defined afterwards)
 		 */
-		for (int i = 0; i < 25; i++) {
+		/*for (int i = 0; i < 25; i++) {
 			testItemList.add(new SpaceShip(random.nextInt(400), random.nextInt(500), 10, marcus));
 			testItem.add(new SpaceShip(random.nextInt(400), random.nextInt(500), 10, arthur));
-		}
-		arthur.setShipsAllPlanets(testItem);
-		marcus.setShipsAllPlanets(testItem);
-		testItemList.add(arthur);
-		testItemList.add(marcus);
+		}*/
+		//arthur.setShipsAllPlanets(testItem);
+		//marcus.setShipsAllPlanets(testItem);
+		/*testItemList.add(arthur);
+		testItemList.add(marcus);*/
+		
+		testItemList.addAll(listPlanets);
 		testItemList.addAll(testItem);
 		Manager manager = new Manager();
-		Run r = new Run(400, 500);
+		Run r = new Run(800, 600);
 
 		/*
 		 * Call the run method of Application providing an initial item Collection, an
--- src/simpleUIApp/Planet.java
+++ src/simpleUIApp/Planet.java
@@ -2,6 +2,9 @@ package simpleUIApp;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.awt.geom.Point2D;
 
 public class Planet extends Entity{
 	
@@ -22,5 +25,55 @@ public class Planet extends Entity{
 	}
 	public void setShipsAllPlanets(ArrayList<SpaceShip> shipsAllPlanets) {
 		this.shipsAllPlanets = shipsAllPlanets;
+	}
+	
+	
+	/**
+	 * @brief found a valid position to place a new planet
+	 * @param width : width of the window
+	 * @param height : height of the window
+	 * @param planets : List of all planet on the map
+	 * @param pos : pos of the new planet to generate
+	 * 
+	 */
+	
+	public static Point2D findPlanetPosition(int width, int height, ArrayList<Planet> planets, int planetSize)
+	{
+		final int nbTry = 1000; // nombre d'essai max pour trouver une pos valide
+		final int spacing = planetSize +30; // on choisi l'espacement entre les planètes
+		boolean isValidPos = true;
+		Random random = new Random();
+		Point2D pos = new Point2D.Double(0,0);
+		
+		int nbTryCpt = 0;
+		
+		do
+		{	
+			int x = random.nextInt((width - (planetSize/2)) + 1 - (planetSize/2)) + (planetSize/2);
+			int y = random.nextInt((height - (planetSize/2)) + 1 - (planetSize/2)) + (planetSize/2);
+			pos.setLocation(x,y);
+			
+			// Si c'est la première planete, son emplacement est forcement valide
+			if(planets.size() == 0)
+				isValidPos = true;
+				
+			
+			for(Planet p : planets){
+				if(p.getLocation().distance(pos) < spacing)
+					isValidPos = false;
+			}
+			
+			nbTryCpt++;
+			
+		} while(!isValidPos && nbTryCpt < nbTry);
+		
+		// Au bout de "nbTry" essais, on arrête de chercher une position valide et on affiche une erreur
+		if(nbTryCpt == nbTry)
+		{
+			System.out.println("Impossible de trouver une position valide pour une nouvelle planete");
+			pos.setLocation(-1, -1);
+		}
+		return pos;
+			
 	}
 }
